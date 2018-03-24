0171b1567a68687b4e52443d6f7cf602cbe61fa0
wyy0920 <wyy0920@gmail.com>
2018-03-24 11:33:44 -0700

    new file

--- Newtwitter.py
+++ Newtwitter.py
@@ -1,9 +1,9 @@
 # Dependencies
 import tweepy
-import time
 import json
-import random
+import time
 import os
+# Added this file
 
 # Twitter API Keys
 consumer_key = os.environ['consumer_key']
@@ -11,33 +11,30 @@ consumer_secret = os.environ['consumer_secret']
 access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 
-# Quotes to Tweet
-happy_quotes = [
-    "For every minute you are angry you lose sixty seconds of happiness. - Ralph Waldo Emerson",
-    "Folks are usually about as happy as they make their minds up to be. - Abraham Lincoln",
-    "Happiness is when what you think, what you say, and what you do are in harmony. - Mahatma Gandhi",
-    "Count your age by friends, not years. Count your life by smiles, not tears. - John Lennon",
-    "Happiness is a warm puppy. - Charles M. Schulz",
-    "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
-    "Now and then it's good to pause in our pursuit of happiness and just be happy. - Guillaume Apollinaire"]
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
-# Create function for tweeting
-def HappyItUp():
-
-    # Twitter credentials
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
-
-    # Tweet a random quote
-    api.update_status(random.choice(happy_quotes))
-
-    # Print success message
-    print("Tweeted successfully, sir!")
+# Create a function that tweets
+def TweetOut(tweet_number):
+    api.update_status(
+        "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
+        tweet_number)
 
 
-# Set timer to run every minute
+# Create a function that calls the TweetOut function every minute
+counter = 0
+
+# Infinitely loop
 while(True):
-    HappyItUp()
+
+    # Call the TweetQuotes function and specify the tweet number
+    TweetOut("Hello Class Second Try {}".format(counter))
+
+    # Once tweeted, wait 60 seconds before doing anything else
     time.sleep(60)
+
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
